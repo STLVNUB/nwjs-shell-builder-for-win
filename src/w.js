a61c170e7b3cb72be01a95752487b1809c99e057
River <revir.qing@gmail.com>
2016-05-12 16:41:05 +0800

    add injected javascript code to src

---
+++ src/w.js
@@ -0,0 +1,37 @@
+(function (require) {
+    var gui = require('nw.gui');
+    var win = gui.Window.get();
+    win.on('resize', function () {
+        win.reload();
+    });
+    if (!win.maximizeInited) {
+        win.maximize();
+        win.maximizeInited = true;
+    }
+
+    function regiestRefreshShortcut(key){
+        //shortcut
+        var option = {
+          key : key,
+          active : function() {
+            win.reload();
+          },
+          failed : function(msg) {
+              // :(, fail to register the |key| or couldn't parse the |key|.
+              console.log(msg);
+            }
+        };
+        var shortcut = new gui.Shortcut(option);
+
+        // Register global desktop shortcut, which can work without focus.
+        gui.App.registerGlobalHotKey(shortcut);
+    }
+
+    win.on("loaded", function () {
+        regiestRefreshShortcut("Ctrl+R");
+        regiestRefreshShortcut("f5");
+    });
+
+
+
+} (require));
